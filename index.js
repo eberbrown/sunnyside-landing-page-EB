3341b05681c46ec7860a7b9693eb49d231f14388
Eber Brown <Eber@MacBook-Pro-de-Eber.local>
2022-11-21 09:50:56 +0100

    final update

--- index.js
+++ index.js
@@ -2,3 +2,30 @@ $(".try1").hover(function () {
     $(this).children().toggleClass("white-background");
 });
 
+var toggled = false;
+
+$(window).resize(() => {
+    const windowObject = $(window);
+    if (toggled === true && windowObject.width() > 991) {
+        $(".li-contact").children().addClass("bg-white text-dark border-white");
+        $(".navbar-nav .nav-item").children().addClass("text-white");
+    } else if (toggled === true && windowObject.width() <= 991) {
+        $(".li-contact").children().removeClass("bg-white text-dark border-white");
+        $(".navbar-nav .nav-item").children().removeClass("text-white");
+    } else {
+        toggled = false;
+    }
+});
+
+$(".navbar-toggler-icon").click(() => {
+    if (toggled === false) {
+        $(".li-contact").children().removeClass("bg-white text-dark border-white");
+        $(".navbar-nav .nav-item").children().removeClass("text-white");
+        toggled = true;
+    } else {
+        $(".li-contact").children().addClass("bg-white text-dark border-white");
+        $(".navbar-nav .nav-item").children().addClass("text-white");
+        toggled = false;
+    }
+});
+
